ba8fc38cb3dc25bbb8e4d4069bbf3147a0b304b9
Marciocheudon <56776494+Marciocheudon@users.noreply.github.com>
2024-09-09 08:05:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd # Para processar a planilha
 
 
-arquivo_excel = 'eleitores2024.csv' # Aqui a gente ta carregando a planilha, caso de erro podemos usar uma LIB de csv
-df = pd.read_excel(arquivo_excel)
+arquivo_csv = 'eleitores2024.csv' # Aqui a gente ta carregando a planilha, caso de erro podemos usar uma LIB de csv
+df = pd.read_csv(arquivo_csv)
 
 
 print(df.head()) # vamos remover essa linha depois, ela serve para printar no console o header da planilha (as primeiras linhas e colunas)
